c567439c9bf32dfe7da0d787c1506d2bed485b0d
Christopher Johnston <chris@fuzzylizard.com>
2025-11-18 14:48:44 -0500

    Adds user button to navbar, can now logout

---
+++ src/components/UserButton.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useSession } from "@/app/(main)/SessionProvider";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { logout } from "@/app/(auth)/actions";
+
+interface UserButtonProps {
+  className?: string;
+}
+
+export default function UserButton({ className }: UserButtonProps) {
+  const { user } = useSession();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className={cn("flex-none rounded-full", className)}>
+          <UserAvatar avatarUrl={user.avatarUrl} size={40} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/users/${user.username}`} className="w-full">
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
