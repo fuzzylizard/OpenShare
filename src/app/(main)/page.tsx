35bd235f8204e401b0b8651f31c21e4078d5ad69
Christopher Johnston <chris@fuzzylizard.com>
2025-11-19 11:13:19 -0500

    Can now create posts, included uI

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,12 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Front Page</main>;
+  return (
+    <main className="w-full">
+      <div className="w-full">
+        <PostEditor />
+      </div>
+    </main>
+  );
 }
